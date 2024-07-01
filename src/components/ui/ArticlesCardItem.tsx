367cf44005b8704e0d32c9d390a405ecfc32e763
Drew Lee <pavlo1031@gmail.com>
2024-07-01 14:23:09 +0800

    (補上修正import方式)

--- src/components/ui/ArticlesCardItem.tsx
+++ src/components/ui/ArticlesCardItem.tsx
@@ -1,6 +1,6 @@
 'use client'
 import Link from 'next/link';
-import UiPropsType from '@/types/UiPropsType';
+import { UiPropsType } from '@/types/UiPropsType';
 
 interface ArticlesCardItemProps extends UiPropsType {
     locked?: boolean

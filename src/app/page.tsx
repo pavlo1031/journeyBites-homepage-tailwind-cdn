f34b93ae2fa1401221a8ea0f82067bba1782e667
Drew Lee <pavlo1031@gmail.com>
2024-07-02 12:22:08 +0800

    左/右側欄 : [left] gap-y 移到前面    [right] 加上 gap-y, px (僅最窄螢幕)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
       <div id='content' className="flex w-full h-fit justify-center py-[80px] gap-x-[24px]">
         {/* <!-- wrapper --> */}
         <div id='content-wrapper' className='relative items-center md:left-[2%] lg:left-[2.5%] xl:left-[2%] flex flex-col md:items-center lg:flex-row lg:justify-center lg:items-start gap-x-[24px] gap-y-[36px] w-full sm:w-[83%] md:w-[85%] lg:w-[88%]'>
-          <div id='left' className='flex flex-col grow w-full sm:max-w-[570px] md:max-w-[unset] md-1:max-w-[535px] md-2:max-w-[unset] lg:w-[unset] lg-1:max-w-[535px] lg-2:max-w-[856px] h-full gap-y-[36px]'>
+          <div id='left' className='flex flex-col gap-y-[36px] grow w-full sm:max-w-[570px] md:max-w-[unset] md-1:max-w-[535px] md-2:max-w-[unset] lg:w-[unset] lg-1:max-w-[535px] lg-2:max-w-[856px] h-full'>
             <PopularArticles>
                 <ArticlesCardItem />
                 <ArticlesCardItem />
@@ -30,7 +30,7 @@ export default function Home() {
                 <ArticlesCardItem />
             </RecommendedArticles>
           </div>
-          <aside id='right' className='flex flex-col gap-y-[36px] w-full sm:max-w-[570px] md:max-w-[unset] md-1:max-w-[535px] md-2:max-w-[unset] lg:w-[350px]'>
+          <aside id='right' className='flex flex-col gap-y-[36px] px-[12px] sm:px-[unset] w-full sm:max-w-[570px] md:max-w-[unset] md-1:max-w-[535px] md-2:max-w-[unset] lg:w-[350px]'>
             <CreatorList>
                 <CreatorListItem />
                 <CreatorListItem />

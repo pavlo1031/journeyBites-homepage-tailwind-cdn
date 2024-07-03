642a43bbf4caa4bd62b981245bb625232354db4c
Drew Lee <pavlo1031@gmail.com>
2024-07-04 03:35:07 +0800

    layout: 文章區塊標題 padding

--- src/components/ui/PopularArticles.tsx
+++ src/components/ui/PopularArticles.tsx
@@ -17,7 +17,7 @@ function PopularArticles(props : PopularArticlesProps) {
     // const { title, description, author, date } = props;
     return (<div id='popular-articles' className={`flex flex-col w-full h-fit bg-[#EEF8FA] ${(className)? className:''}`} style={{...style}}>
         <div className='flex flex-row justify-between w-full h-fit sm:h-[88px]'>
-            <div className='ml-[12px] sm:ml-0 flex flex-center w-[163px] h-[52px] sm:w-[200px] sm:h-[64px] gap-x-[12px] sm:gap-x-[4px] text-white text-[24px] leading-[32px] rounded-b-[7px] sm:rounded-tl-[7px] sm:border-r-[7px] sm:border-b-[7px] sm:border-white bg-[#59ABBD]'>
+            <div className='ml-[12px] sm:ml-0 p-[12px_24px] sm:p-[16px_32px] flex flex-center w-[163px] h-[52px] sm:w-[200px] sm:h-[64px] gap-x-[12px] sm:gap-x-[4px] text-white text-[24px] leading-[32px] rounded-b-[7px] sm:rounded-tl-[7px] sm:border-r-[7px] sm:border-b-[7px] sm:border-white bg-[#59ABBD]'>
                 <i className="size-[24px] bg-icon-party-popper"></i>
                 <h3 className='text-[20px] sm:text-[24px] w-[83px] h-[28px] sm:w-[100px] sm:h-[32px]'>熱門文章</h3>
             </div>

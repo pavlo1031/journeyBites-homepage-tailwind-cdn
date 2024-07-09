80921a8c1c2294d40e1a2a4f0a104da765bbcf5f
Drew Lee <pavlo1031@gmail.com>
2024-07-09 08:08:43 +0800

    style: 「熱門文章」「推薦文章」設定最小高度 (min height)

--- src/components/ui/PopularArticles.tsx
+++ src/components/ui/PopularArticles.tsx
@@ -16,7 +16,7 @@ const PopularArticles: React.FC<PopularArticlesProps> = (props : PopularArticles
     const { children = [] } = props;
     const { id, className, style } = props;
     return (<div id='popular-articles' className={`flex flex-col w-full h-fit bg-[#EEF8FA] ${(className)? className:''}`} style={{...style}}>
-        <div className='flex flex-row justify-between w-full h-fit sm:h-[88px]'>
+        <div className='flex flex-row justify-between w-full h-fit min-h-[250px] sm:h-[88px]'>
             <div className='ml-[12px] sm:ml-0 p-[12px_24px] sm:p-[16px_32px] flex flex-center w-[163px] h-[52px] sm:w-[200px] sm:h-[64px] gap-x-[12px] sm:gap-x-[4px] text-white text-[24px] leading-[32px] rounded-b-[7px] sm:rounded-tl-[7px] sm:border-r-[7px] sm:border-b-[7px] sm:border-white bg-[#59ABBD]'>
                 <i className="size-[24px] bg-icon-party-popper"></i>
                 <h3 className='text-[20px] sm:text-[24px] w-[83px] h-[28px] sm:w-[100px] sm:h-[32px]'>熱門文章</h3>
--- src/components/ui/RecommendedArticles.tsx
+++ src/components/ui/RecommendedArticles.tsx
@@ -12,7 +12,7 @@ const RecommendedArticles: React.FC<RecommendedArticlesProp> = (props: Recommend
     const { children = [] } = props;
     const { id, className, style } = props;
     return (<div id='recommended-articles' className={`flex flex-col w-full h-fit bg-[#EBEFFA] ${(className)? className:''}`} style={{...style}}>
-        <div className='flex flex-row justify-between w-full h-fit sm:h-[88px]'>
+        <div className='flex flex-row justify-between w-full h-fit min-h-[250px] sm:h-[88px]'>
             <div className='ml-[12px] sm:ml-0 p-[12px_24px] sm:p-[16px_32px] flex flex-center w-[163px] h-[52px] sm:w-[200px] sm:h-[64px] gap-x-[12px] sm:gap-x-[4px] text-white text-[24px] leading-[32px] rounded-b-[7px] sm:rounded-tl-[7px] sm:border-r-[7px] sm:border-b-[7px] sm:border-white bg-[#444FAE]'>
                 <i className="size-[24px] bg-icon-thumb-up"></i>
                 <h3 className='text-[20px] sm:text-[24px] w-[83px] h-[28px] sm:w-[100px] sm:h-[32px]'>推薦文章</h3>

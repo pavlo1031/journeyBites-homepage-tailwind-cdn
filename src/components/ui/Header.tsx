68e3603610399363819cc915e3481749f1838be5
Drew Lee <pavlo1031@gmail.com>
2024-07-09 20:06:03 +0800

    style: [Header] 調整 sizes、font size、line height、...

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -8,11 +8,11 @@ interface HeaderProps extends UiPropsType {
 function Header(props: HeaderProps) {
     const { className, style } = props;
     return (<header className={`fixed shadow-md z-10 top-0 flex flex-row justify-between w-full h-[84px] py-[20px] bg-white ${(className)? className:''}`} style={{...style}}>
-        <div className='flex flex-row justify-between items-center w-full'>
+        <div className='flex flex-row justify-between items-center w-full h-fit'>
         <div className='flex flex-row justify-between items-center w-fit ml-[16px] sm:ml-[30px] md:ml-[16.25%]'>
             <img src='logo-sm.svg' className='object-cover w-[160px] h-[20px] lg:w-[199px] lg:h-[26px] ml-[16px]' alt='jouney bites' />
-            <div className='ml-[13%] text-[25px] font-[700] text-nowrap leading-7 hidden lg:block'>探索</div>
-            <i className='bg-icon-search size-[24px] sm:ml-[15px] ml-[9%] shrink-0 hidden sm:block' />
+            <div className='ml-[13%] text-[20px] font-[700] text-nowrap leading-[28px] hidden lg:block'>探索</div>
+            <i className='bg-icon-search size-[24px] sm:ml-[32px] shrink-0 hidden sm:block' />
         </div>
         <div className='flex flex-row items-center h-[44px] gap-x-[32px] 2xs:gap-x-[24px] mr-[12px] sm:mr-[30px] md-2:mr-[16.25%]'>
             <button className='text-base text-white text-nowrap rounded-md w-[99px] h-[44px] bg-[#444FAE] hidden sm-2:block'>開始創作</button>

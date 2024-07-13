eddaffc637c9f8041beb929ee805174bef466ceb
Drew Lee <pavlo1031@gmail.com>
2024-07-13 18:32:06 +0800

    code format: <div>縮排結構

--- src/components/ui/HamburgerMenu.tsx
+++ src/components/ui/HamburgerMenu.tsx
@@ -22,26 +22,25 @@ function HamburgerMenu(props: HamburgerMenuProps) {
             <button id='btn-closeMenu' className='ml-auto text-[20px]' onClick={handleOnClose}>×</button>
         </div>
         <div className='flex flex-col justify-start w-full h-fit'>
-        <div id='menu-searchbar' className='flex flex-row items-center gap-x-[12px] p-[10px_12px] h-[44px] my-[8px] bg-white rounded-[8px] border border-[#E2E8F0]'>
-            <i className='bg-icon-search text-[#444FAE] size-[24px] shrink-0'></i>
-            <input contentEditable className='focus:outline-0 w-full ' placeholder='Search'></input>
+            <div id='menu-searchbar' className='flex flex-row items-center gap-x-[12px] p-[10px_12px] h-[44px] my-[8px] bg-white rounded-[8px] border border-[#E2E8F0]'>
+                <i className='bg-icon-search text-[#444FAE] size-[24px] shrink-0'></i>
+                <input contentEditable className='focus:outline-0 w-full ' placeholder='Search'></input>
+            </div>
+            <div className='p-[8px] gap-y-[4px]'>
+                <div className='h-[28px] font-[700] text-[#444FAE]'>探索</div>
+                <ul className='item-container w-full h-fit font-[400]'>
+                    <li className='h-[44px] active:bg-[#444FAE] active:text-white active:rounded-[8px] p-[8px]'><Link href='#' className='w-full block'>熱門文章</Link></li>
+                    <li className='h-[44px] active:bg-[#444FAE] active:text-white active:rounded-[8px] p-[8px]'><Link href='#' className='w-full block'>最新文章</Link></li>
+                    <li className='h-[44px] active:bg-[#444FAE] active:text-white active:rounded-[8px] p-[8px]'><Link href='#' className='w-full block'>所有分類</Link></li>
+                    <li className='h-[44px] active:bg-[#444FAE] active:text-white active:rounded-[8px] p-[8px]'><Link href='#' className='w-full block'>台灣旅遊地圖</Link></li>
+                    <li className='h-[44px] active:bg-[#444FAE] active:text-white active:rounded-[8px] p-[8px]'><Link href='#' className='w-full block'>步道旅行</Link></li>
+                    <li className='h-[44px] active:bg-[#444FAE] active:text-white active:rounded-[8px] p-[8px]'><Link href='#' className='w-full block'>健行日記</Link></li>
+                    <li className='h-[44px] active:bg-[#444FAE] active:text-white active:rounded-[8px] p-[8px]'><Link href='#' className='w-full block'>創作者列表</Link></li>
+                    <li className='h-[44px] active:bg-[#444FAE] active:text-white active:rounded-[8px] p-[8px]'><Link href='#' className='w-full block'>旅遊食記</Link></li>
+                    <li className='h-[44px] active:bg-[#444FAE] active:text-white active:rounded-[8px] p-[8px]'><Link href='#' className='w-full block'>台灣百岳</Link></li>
+                </ul>
+            </div>
         </div>
-        <div className='p-[8px] gap-y-[4px]'>
-            <div className='h-[28px] font-[700] text-[#444FAE]'>探索</div>
-            <ul className='item-container w-full h-fit font-[400]'>
-                <li className='h-[44px] active:bg-[#444FAE] active:text-white active:rounded-[8px] p-[8px]'><Link href='#' className='w-full block'>熱門文章</Link></li>
-                <li className='h-[44px] active:bg-[#444FAE] active:text-white active:rounded-[8px] p-[8px]'><Link href='#' className='w-full block'>最新文章</Link></li>
-                <li className='h-[44px] active:bg-[#444FAE] active:text-white active:rounded-[8px] p-[8px]'><Link href='#' className='w-full block'>所有分類</Link></li>
-                <li className='h-[44px] active:bg-[#444FAE] active:text-white active:rounded-[8px] p-[8px]'><Link href='#' className='w-full block'>台灣旅遊地圖</Link></li>
-                <li className='h-[44px] active:bg-[#444FAE] active:text-white active:rounded-[8px] p-[8px]'><Link href='#' className='w-full block'>步道旅行</Link></li>
-                <li className='h-[44px] active:bg-[#444FAE] active:text-white active:rounded-[8px] p-[8px]'><Link href='#' className='w-full block'>健行日記</Link></li>
-                <li className='h-[44px] active:bg-[#444FAE] active:text-white active:rounded-[8px] p-[8px]'><Link href='#' className='w-full block'>創作者列表</Link></li>
-                <li className='h-[44px] active:bg-[#444FAE] active:text-white active:rounded-[8px] p-[8px]'><Link href='#' className='w-full block'>旅遊食記</Link></li>
-                <li className='h-[44px] active:bg-[#444FAE] active:text-white active:rounded-[8px] p-[8px]'><Link href='#' className='w-full block'>台灣百岳</Link></li>
-            </ul>
-        </div>
-        </div>
-
         <div id='menu-buttons-guest' className='mt-auto flex flex-row gap-x-[24px] w-full h-fit'>
             <button className='w-[calc((100%-24px)/2)] h-[44px] rounded-[6px] font-[700] text-[#444FAE] border border-2 border-[#444FAE]'>登入</button>
             <button className='w-[calc((100%-24px)/2)] h-[44px] rounded-[6px] font-[700] text-[#EBEFFA] bg-[#444FAE] border border-2 border-[#444FAE]'>註冊</button>
--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -9,17 +9,17 @@ function Header(props: HeaderProps) {
     const { className, style } = props;
     return (<header className={`fixed shadow-md z-10 top-0 flex flex-row justify-between w-full h-[84px] py-[20px] bg-white ${(className)? className:''}`} style={{...style}}>
         <div className='flex flex-row justify-between items-center w-full h-fit'>
-        <div className='flex flex-row justify-between items-center w-fit ml-[16px] sm:ml-[30px] md:ml-[16.25%]'>
-            <img src='logo-sm.svg' className='object-cover w-[160px] h-[20px] lg:w-[199px] lg:h-[26px] ml-[16px]' alt='jouney bites' />
-            <div className='ml-[13%] text-[20px] font-[700] text-nowrap leading-[28px] hidden lg:block'>探索</div>
-            <i className='bg-icon-search size-[24px] sm:ml-[32px] shrink-0 hidden sm:block' />
-        </div>
-        <div className='flex flex-row items-center h-[44px] gap-x-[32px] 2xs:gap-x-[24px] mr-[12px] sm:mr-[30px] md-2:mr-[16.25%]'>
-            <button className='text-base text-white text-nowrap rounded-md w-[99px] h-[44px] bg-[#444FAE] hidden sm-2:block'>開始創作</button>
-            <Link href='#' className='bg-icon-bell size-[24px]' title='通知' />
-            <Link href='#' className='bg-icon-user size-[24px] 2xs:ml-[12px]' title='user' />
-            <Link id='btn-openMenu' href='#' className='bg-icon-menu size-[24px] 2xs:ml-[12px] 2xs:block sm:hidden' title='menu' />
-        </div>
+            <div className='flex flex-row justify-between items-center w-fit ml-[16px] sm:ml-[30px] md:ml-[16.25%]'>
+                <img src='logo-sm.svg' className='object-cover w-[160px] h-[20px] lg:w-[199px] lg:h-[26px] ml-[16px]' alt='jouney bites' />
+                <div className='ml-[13%] text-[20px] font-[700] text-nowrap leading-[28px] hidden lg:block'>探索</div>
+                <i className='bg-icon-search size-[24px] sm:ml-[32px] shrink-0 hidden sm:block' />
+            </div>
+            <div className='flex flex-row items-center h-[44px] gap-x-[32px] 2xs:gap-x-[24px] mr-[12px] sm:mr-[30px] md-2:mr-[16.25%]'>
+                <button className='text-base text-white text-nowrap rounded-md w-[99px] h-[44px] bg-[#444FAE] hidden sm-2:block'>開始創作</button>
+                <Link href='#' className='bg-icon-bell size-[24px]' title='通知' />
+                <Link href='#' className='bg-icon-user size-[24px] 2xs:ml-[12px]' title='user' />
+                <Link id='btn-openMenu' href='#' className='bg-icon-menu size-[24px] 2xs:ml-[12px] 2xs:block sm:hidden' title='menu' />
+            </div>
         </div>
     </header>);
 }

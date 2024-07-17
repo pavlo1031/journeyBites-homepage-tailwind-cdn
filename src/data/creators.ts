aaa378dec2235f94162d7e78b5452a8a11ce8c65
Drew Lee <pavlo1031@gmail.com>
2024-07-17 20:30:22 +0800

    更新: 未來的動態資料 (article、author、creator)

--- src/data/creators.ts
+++ src/data/creators.ts
@@ -3,35 +3,35 @@ import { CreatorData } from "@/model/CreatorData";
 export const data: CreatorData[] = [
     {
         'id': '001',
-        'imageURL': 'https://s3-alpha-sig.figma.com/img/7a5e/7158/b8401d2633ab45ca0d80dccd87cba41e?Expires=1721001600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=TiUlbYcyKcnXkcCuC-lUp73DckI4E7Vd3ptgfS2FcmfTvuvvDJs9ZSCLs~Z-IEuoqk4DODJ7e1N6U4ujs2AJ1ydyfyH~xQXGO97qK3aAVoTAsl0423X9C9wsRTj7A4LzmaciaLAfsvq0YS7x5xkBs9rmHfucGfn8J61G~UJw27i8l1tfbPBzYdpP0GpFnFMCa4XQL4L7nQjucy5NRBBrnBH6iW~xTbKViAgugSjmz3u1jLyneh21ImhaktqkcnTmm8DY-PIwhLAn8r8ZDFoLe5MNn7DwTvTzjd7nT2cNFb5QccaKugBAbT66zeNrtVwYXDFv1PhislKQtuNiCyPT-g__',
+        'imageURL': 'https://s3-alpha-sig.figma.com/img/7a5e/7158/b8401d2633ab45ca0d80dccd87cba41e?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=msUARSmxXb4PCjDU6h2NLHWm-YJFccyxo-P9xyWSGdS5GCBD-4fY1N5b0H9RTGoLSYY4PDzx4hZxQNLdUJK~vK3iRFiLpUNUq-wBcrWSoMiXNtO9o7Ly7C2Rcscg0n-QKaj4qMPIkFsuZEzH~jpaXMPQJ0I~yxw3fu5wOex-90-2tS03PfiTagwR-VcykzB5ZdWgudyt9TJwpiKl-jV~9RvyAqTkoDg20fZPSm-yjqqOsCQxI-G-f-HsS301y9oT0Aisa-r0JbhzQUbUiqxtn~LW-L8ax9aKxhehOoMtOF5evzARIb~cbt2hBobJAvkoKd-FEofCEhA6Imyi~I~5jQ__',
         'imageType': 'jfif',
         'name': '瑪雅·弗朗西斯',
         'description': '瑪雅是一位對冒險充滿熱情的旅行者。她喜歡挑戰自己,探索世...'
     },
     {
         'id': '002',
-        'imageURL': 'https://s3-alpha-sig.figma.com/img/fc5d/4944/a9aff57fe1e8c98d949c6bc986640c73?Expires=1721001600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=iU774CTu~TDjUUVRNrNRP1-IGQUqqHGFr7HPFHYn5UP~71tzLaj50nEFm1d2Ctf9jZTKDILXRQGmlzt84vbb~~b7SMmXjKPYkqvKa1pEMmdhqWQh6kVtHmnxqOD21dVq~6eLX9ZH2SiA0Ava-PLznIBoTZ~5K1fnt1E-~EPTz9LvAME7kKKhIzSoL1KSf2cumpqxWGNNpSLLjLKxPlTIDuXYeMpEobQ9OxlpQgZWdE1FwgbRQnWtdd3HqLV2r7XlLt7c5LmlyiEv1nXOdpKkfhfGXAI61nbrdMY1iHcAuXsWcuLL9Qs~wKfOpAe9-9lTZE0KIMVDi-VOLKggP2htYg__',
+        'imageURL': 'https://s3-alpha-sig.figma.com/img/fc5d/4944/a9aff57fe1e8c98d949c6bc986640c73?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=l0JTUdj1Qol70UGKmnjbqqeV8G97Ll2wXLl-51u20l-3k0dBplNVsCGS9Co3xFJy5nlpKuM5yJp0HyxJXxlnRmbLJSINc0n0sNByrgtAuU6mecvQwGJM6HKlqHcHZzFqYD4CY0UGM7NRWG3JBeFX02s1Txp103GNRUZ-7YKPR5V5eZBbGWpIsPmOoJkrGC3TkcDsNfT7xCc7HcrjtOx0j1V1FhDBGGleHuc~o3fE8frz1koi0jqzcrX0v~zxALgqt4NC0T7pgyOyTJggH-XKGfUo9Po6UBb34UiTujUMJSB5mvVvJhNOqVXAJUYcZ~1cRs-M~LFLufW6KyND-KiYVA__',
         'imageType': 'jfif',
         'name': '卡洛斯·羅德理格',
         'description': '洛斯是一位熱愛攝影和探險的背包客。他總是帶著相機和行囊,探索...'
     },
     {
         'id': '003',
-        'imageURL': 'https://s3-alpha-sig.figma.com/img/ebda/2b32/0a55a076e87205c1c38c92456d771335?Expires=1721001600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=HHlmCMlJvYwlA-VW96HCM9RZq9wb~SDzqp-Arzz1IiPNA7bSwc8a18qlLibKI5XT7D8VR7omFaKDFJ--S5ztmK9vXZDcr6C68QEaqrERkF0xd4Pg1-HWvPM7p-smDNLaKNGTZ-90xj3jJt3Rl1WNpUvJqS8sJQl~93LfI9LOC4lPczIc6Ni8vYQ262aQ5XrovWAsn2WSV1lWxIULiAAfAfngrYgx7efmi6tx0gc-Y2BuHY4eTXYpGdxL-PC9NO1Eb969GVohHxcRFRCOjXK3Zya-iqtATa-dA4dfbPgZyLdCKoY~ObEvbn-AfLyAkoLm6NIji0lZ9vNfpv2kGieIKA__',
+        'imageURL': 'https://s3-alpha-sig.figma.com/img/ebda/2b32/0a55a076e87205c1c38c92456d771335?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=djIiVmkR7prYXSZVp9bNvplwcHFvteK4z6dt~PNYJCzg3Qp1si0KxJ-Us0IUEKnPPaHoEjksL~NrC97rdIslmx7eQZ-fuGBh4sRrDcoLqbkFVjxJj3re4e~TfX1kg2Z4HWCJTCRkIjEKO61CbymMmq854gGwSdkn~pqKmcGUySxCUyrUF8UfjFtUxsnuPzjyu0bK1yWi2sHSBvlmMdGZ-YIy2LqiUO4OXB-N7v7eFri5oUB2ZkeTKeSAsPB1XqlmGs6iyjr2OUcgsoXI1iOOggmolTGR3NMJUh-vuVZa927yPRDhtii7V9E357CBddywdH8j-DsL9YpjTmP1aEafBQ__',
         'imageType': 'jfif',
         'name': '艾米莉亞·沃爾什',
         'description': '艾米莉亞是一位文化探險家,對於世界各地的古老建築和歷史遺產充...'
     },
     {
         'id': '004',
-        'imageURL': 'https://s3-alpha-sig.figma.com/img/a486/99e6/365ee05c12af95e5a7358195d18f5e91?Expires=1721001600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Snj5bDQYaZ4QUpKTUp7PRCl1grGDVdmfEXD~Wztxpid0ogblYVEZuLq~tM05LhqrInK6zP1xk1ADqFgdI-EcQx55qDVxQWVNtEk0DGbi09AXu2lwkd6DGs6YId0hKWrAizzfSu5HQYOSXvO9Y2T6SoQqLP0fEuAvN0LjJhKIMQC3BB1IIgaJ5geP73CaEIte1JgKed0P3-6mxaZnlieBJkjM4S9lSlshjcgpY8-vuILSmwo-qd4Zs6wc6dFNy-IhT2v~ZAs3UxVaw~DPVsAzq8o3RbdEfki8wJIgMNfBAlptpxJUo85DDLW5oNWEORwP45HbnckF1MumAxDN6ruBpA__',
+        'imageURL': 'https://s3-alpha-sig.figma.com/img/a486/99e6/365ee05c12af95e5a7358195d18f5e91?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=BkwAOj3H21kPd9Uwkl68N9UpFJf2iXt7QC5ik7CkpgV66DFO-0e9AKAo4O5SO956aCMMa-0ml~oTDlZotrVwXK7NC4Eu5ka5ROJ8mgUrlGM-BBr6d5zMmmUYCEfjSJodp2mawskE4YsUptsr3D5ErRnAmubv16FvTrKKJ35djlS9QJTYRJAArRa1NS5QR~kyNYPsV8Fwb71RwWG4~DU~tgX69mDpo4dkF5psBnGfCDJWKh-pNrNR1lTBYPokCjCud3ROe5~Tf0InVJcgFQYyY5v0j3GZ7d0FC6PXFnPoH2RWki2hi89xqHs63~5sPaBRXjZ8-mYwkDShAsLV83gmCw__',
         'imageType': 'jfif',
         'name': '迪戈·阿爾瓦雷斯',
         'description': '迪戈是一位愛好自然和生態旅行的探險家。他喜歡徒步穿越山川和...'
     },
     {
         'id': '005',
-        'imageURL': 'https://s3-alpha-sig.figma.com/img/67d7/4a77/fe991fffafdeb37272f706b12f8e938f?Expires=1721001600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=fVdRCc55hIeepzVuznDuMJSdxTvApkfAq3Tqvc--Ue8jscMpbSy0nZxxi0Z-oVDO4JzW7FeLHLQP4Qdc8qM5JnpXjM8LkqX62GD~EdYmmgICGL7eIEjRm5CkcChv2IDmmnRQd20Cqm4~69sc6fDYGvBZakJRHXzGxGTLSNvv7VI8sESZUXVXQGe0hdfPE7UjQnrQhs46K4tFaqkww1bI3gCW-Fz6kYxn9VlAbeMD962~RJBVMzPmwkzCWUuu8OO4M4fAMovJnSQFigsz3Y~lAEPGaKRL3amrMNVtzMKNSdcdq-w3xRGfs6MlaLR36n7iBUXCP09tuSSsi1P7wUwKtw__',
+        'imageURL': 'https://s3-alpha-sig.figma.com/img/67d7/4a77/fe991fffafdeb37272f706b12f8e938f?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=NE60o-RLgmS2Z3yUKA6Ht3XG9VxR9s5Q9YxifAoqC5U6Ez9MAnKHcWnpPI1Ah9dUgEkumhii96A7xXsb~OcYf0IUphkDkM5sB7q6YrI0H2fA-xJNU39CaAICSCNqu6sIYA9Bek4x2u2I~YK6oZn2eXhyGysNcgwPJZtCVfg4v-jvW-F1cFI5tPaT9~txf7x5qY3G9ySWgj1R2H-07~ZsRzZkIKzFGXWbcojPbE6ATpNVgvwc6dwl9co6~SWnoit2klOi7ICzoqK-If~fnVwqaeOInsK7v6N~5Kzcm2U43ku9ZlK94J3B8FswVvH6j5WlFvPd4DEeC1QiDyW8YXVYag__',
         'imageType': 'jfif',
         'name': '莉莎·安德森',
         'description': '莉莎是一位瑜伽和靈性旅行者,對於身心靈平衡充滿追求。'

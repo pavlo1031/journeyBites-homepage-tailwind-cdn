0bd307825b4f5e4e1834fc6e1cc53318d2386f1c
Drew Lee <pavlo1031@gmail.com>
2024-07-22 11:27:48 +0800

    refactor: (1).移除id    (2).隱藏 "已登入使用者" 的 buttons

--- src/components/ui/HamburgerMenu.tsx
+++ src/components/ui/HamburgerMenu.tsx
@@ -23,7 +23,8 @@ function HamburgerMenu(props: HamburgerMenuProps) {
             <button id='btn-closeMenu' className='ml-auto text-[20px]' onClick={handleOnClose}>×</button>
         </div>
         <div className='flex flex-col justify-start w-full h-fit'>
-            <div id='menu-searchbar' className='flex flex-row items-center gap-x-[12px] p-[10px_12px] h-[44px] my-[8px] bg-white rounded-[8px] border border-[#E2E8F0]'>
+            {/* Menu searchbar */}
+            <div className='flex flex-row items-center gap-x-[12px] p-[10px_12px] h-[44px] my-[8px] bg-white rounded-[8px] border border-[#E2E8F0]'>
                 <i className='bg-icon-search text-[#444FAE] size-[24px] shrink-0'></i>
                 <input contentEditable className='focus:outline-0 w-full ' placeholder='Search'></input>
             </div>
@@ -42,11 +43,13 @@ function HamburgerMenu(props: HamburgerMenuProps) {
                 </ul>
             </div>
         </div>
-        <div id='menu-buttons-guest' className='mt-auto flex flex-row gap-x-[24px] w-full h-fit'>
+        {/* for guests */}
+        <div className='mt-auto flex flex-row gap-x-[24px] w-full h-fit'>
             <button className='w-[calc((100%-24px)/2)] h-[44px] rounded-[6px] font-[700] text-[#444FAE] border border-2 border-[#444FAE]'>登入</button>
             <button className='w-[calc((100%-24px)/2)] h-[44px] rounded-[6px] font-[700] text-[#EBEFFA] bg-[#444FAE] border border-2 border-[#444FAE]'>註冊</button>
         </div>
-        <div id='menu-buttons-login' className='flex flex-row gap-x-[24px] w-full h-fit'>
+        {/* for login user */}
+        <div className='flex flex-row gap-x-[24px] w-full h-fit hidden'>
             <button className='w-full h-[44px] rounded-[6px] font-[700] text-[#EBEFFA] bg-[#444FAE] border border-2 border-[#444FAE]'>開始</button>
         </div>
   </div>);

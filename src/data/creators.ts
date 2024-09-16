b8ab152ee8bd8996f36aaff6a8f789ab77b26640
Drew Lee <pavlo1031@gmail.com>
2024-09-16 23:48:17 +0800

    data: 更新樣本資料 imageURL

--- src/data/creators.ts
+++ src/data/creators.ts
@@ -3,35 +3,35 @@ import { CreatorData } from "@/model/CreatorData";
 export const data: CreatorData[] = [
     {
         'id': '001',
-        'imageURL': 'https://s3-alpha-sig.figma.com/img/7a5e/7158/b8401d2633ab45ca0d80dccd87cba41e?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=msUARSmxXb4PCjDU6h2NLHWm-YJFccyxo-P9xyWSGdS5GCBD-4fY1N5b0H9RTGoLSYY4PDzx4hZxQNLdUJK~vK3iRFiLpUNUq-wBcrWSoMiXNtO9o7Ly7C2Rcscg0n-QKaj4qMPIkFsuZEzH~jpaXMPQJ0I~yxw3fu5wOex-90-2tS03PfiTagwR-VcykzB5ZdWgudyt9TJwpiKl-jV~9RvyAqTkoDg20fZPSm-yjqqOsCQxI-G-f-HsS301y9oT0Aisa-r0JbhzQUbUiqxtn~LW-L8ax9aKxhehOoMtOF5evzARIb~cbt2hBobJAvkoKd-FEofCEhA6Imyi~I~5jQ__',
+        'imageURL': 'https://scontent.ftpe8-3.fna.fbcdn.net/v/t1.6435-9/192178309_325862918910264_6988647392474304492_n.jpg?stp=dst-jpg_s960x960&_nc_cat=107&ccb=1-7&_nc_sid=2a1932&_nc_ohc=cKNcyoaT_owQ7kNvgHfrr6q&_nc_ht=scontent.ftpe8-3.fna&_nc_gid=A4qEMAGBMw5F0kWbZpNoc2s&oh=00_AYCmh_7gPZwrTRFajwmKp2DRcC0MLAr6gZmtWwJl4LBmVQ&oe=670BBE6C',
         'imageType': 'jfif',
         'name': '瑪雅·弗朗西斯',
         'description': '瑪雅是一位對冒險充滿熱情的旅行者。她喜歡挑戰自己,探索世...'
     },
     {
         'id': '002',
-        'imageURL': 'https://s3-alpha-sig.figma.com/img/fc5d/4944/a9aff57fe1e8c98d949c6bc986640c73?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=l0JTUdj1Qol70UGKmnjbqqeV8G97Ll2wXLl-51u20l-3k0dBplNVsCGS9Co3xFJy5nlpKuM5yJp0HyxJXxlnRmbLJSINc0n0sNByrgtAuU6mecvQwGJM6HKlqHcHZzFqYD4CY0UGM7NRWG3JBeFX02s1Txp103GNRUZ-7YKPR5V5eZBbGWpIsPmOoJkrGC3TkcDsNfT7xCc7HcrjtOx0j1V1FhDBGGleHuc~o3fE8frz1koi0jqzcrX0v~zxALgqt4NC0T7pgyOyTJggH-XKGfUo9Po6UBb34UiTujUMJSB5mvVvJhNOqVXAJUYcZ~1cRs-M~LFLufW6KyND-KiYVA__',
+        'imageURL': 'https://ichef.bbci.co.uk/ace/ws/800/cpsprodpb/17E11/production/_113790879_taylor-swift-new-album-tout.jpg.webp',
         'imageType': 'jfif',
         'name': '卡洛斯·羅德理格',
         'description': '洛斯是一位熱愛攝影和探險的背包客。他總是帶著相機和行囊,探索...'
     },
     {
         'id': '003',
-        'imageURL': 'https://s3-alpha-sig.figma.com/img/ebda/2b32/0a55a076e87205c1c38c92456d771335?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=djIiVmkR7prYXSZVp9bNvplwcHFvteK4z6dt~PNYJCzg3Qp1si0KxJ-Us0IUEKnPPaHoEjksL~NrC97rdIslmx7eQZ-fuGBh4sRrDcoLqbkFVjxJj3re4e~TfX1kg2Z4HWCJTCRkIjEKO61CbymMmq854gGwSdkn~pqKmcGUySxCUyrUF8UfjFtUxsnuPzjyu0bK1yWi2sHSBvlmMdGZ-YIy2LqiUO4OXB-N7v7eFri5oUB2ZkeTKeSAsPB1XqlmGs6iyjr2OUcgsoXI1iOOggmolTGR3NMJUh-vuVZa927yPRDhtii7V9E357CBddywdH8j-DsL9YpjTmP1aEafBQ__',
+        'imageURL': 'https://images.unsplash.com/photo-1506126613408-eca07ce68773?q=80&w=1399&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         'imageType': 'jfif',
         'name': '艾米莉亞·沃爾什',
         'description': '艾米莉亞是一位文化探險家,對於世界各地的古老建築和歷史遺產充...'
     },
     {
         'id': '004',
-        'imageURL': 'https://s3-alpha-sig.figma.com/img/a486/99e6/365ee05c12af95e5a7358195d18f5e91?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=BkwAOj3H21kPd9Uwkl68N9UpFJf2iXt7QC5ik7CkpgV66DFO-0e9AKAo4O5SO956aCMMa-0ml~oTDlZotrVwXK7NC4Eu5ka5ROJ8mgUrlGM-BBr6d5zMmmUYCEfjSJodp2mawskE4YsUptsr3D5ErRnAmubv16FvTrKKJ35djlS9QJTYRJAArRa1NS5QR~kyNYPsV8Fwb71RwWG4~DU~tgX69mDpo4dkF5psBnGfCDJWKh-pNrNR1lTBYPokCjCud3ROe5~Tf0InVJcgFQYyY5v0j3GZ7d0FC6PXFnPoH2RWki2hi89xqHs63~5sPaBRXjZ8-mYwkDShAsLV83gmCw__',
+        'imageURL': 'https://cdn.britannica.com/55/188355-050-D5E49258/Salvatore-Corsitto-The-Godfather-Marlon-Brando-Francis.jpg',
         'imageType': 'jfif',
         'name': '迪戈·阿爾瓦雷斯',
         'description': '迪戈是一位愛好自然和生態旅行的探險家。他喜歡徒步穿越山川和...'
     },
     {
         'id': '005',
-        'imageURL': 'https://s3-alpha-sig.figma.com/img/67d7/4a77/fe991fffafdeb37272f706b12f8e938f?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=NE60o-RLgmS2Z3yUKA6Ht3XG9VxR9s5Q9YxifAoqC5U6Ez9MAnKHcWnpPI1Ah9dUgEkumhii96A7xXsb~OcYf0IUphkDkM5sB7q6YrI0H2fA-xJNU39CaAICSCNqu6sIYA9Bek4x2u2I~YK6oZn2eXhyGysNcgwPJZtCVfg4v-jvW-F1cFI5tPaT9~txf7x5qY3G9ySWgj1R2H-07~ZsRzZkIKzFGXWbcojPbE6ATpNVgvwc6dwl9co6~SWnoit2klOi7ICzoqK-If~fnVwqaeOInsK7v6N~5Kzcm2U43ku9ZlK94J3B8FswVvH6j5WlFvPd4DEeC1QiDyW8YXVYag__',
+        'imageURL': 'https://2.bp.blogspot.com/-o_4kiv9X8T8/Vm1g5e7lH-I/AAAAAAAAPKQ/ebNOoQWjPsE/s200/1.png',
         'imageType': 'jfif',
         'name': '莉莎·安德森',
         'description': '莉莎是一位瑜伽和靈性旅行者,對於身心靈平衡充滿追求。'
